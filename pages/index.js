6cfb217b84d19a3f31b7bb35265a25832b77f7f1
zchuhui <943339564@qq.com>
2018-01-06 15:43:02 +0800

    styles

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,29 @@ const Index = (props) => (
       ))
       }
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
 
