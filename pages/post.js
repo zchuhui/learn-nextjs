1e60bd9a8792ce63e04fc44fded2f5652e2f5cf5
zchuhui <943339564@qq.com>
2018-01-06 15:05:41 +0800

    fetching data

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,26 @@
 import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
 
-export default (props) => (
+const Post = (props) => (
     <Layout>
-       <h1>{props.url.query.title}</h1>
-       <p>This is the blog post content.</p>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium}/>
     </Layout>
 )
+
+Post.getInitialProps = async (context) =>{
+    console.log(context.query);
+    const {id} = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const data = await res.json();
+
+    console.log(`Fetched show: ${data.name}`)
+
+    return {
+        show:data
+    }
+}
+
+export default Post
+
