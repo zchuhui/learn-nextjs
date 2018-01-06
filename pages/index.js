5231e302c2a23025cbbe1d3978369deee06ba688
zchuhui <943339564@qq.com>
2018-01-06 10:39:56 +0800

    use dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 export default () => (
-    <Layout>
-       <p>Hello Next.js</p>
-    </Layout>
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello nextJs" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+    <p>Hello Next.js</p>
+  </Layout>
 )
